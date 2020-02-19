89508a0d41af517248fabfc73430c04704c4149e
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-19 02:58:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+  delete newObject.key
+
 }
